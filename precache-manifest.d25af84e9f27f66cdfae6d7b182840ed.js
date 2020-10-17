ba175316604ea8984bce70de629d0dcc85f4a180
harshiiiz <harshitasahu3110@gmail.com>
2020-10-17 21:12:30 +0530

    Updates

---
+++ precache-manifest.d25af84e9f27f66cdfae6d7b182840ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e365637171529223911c93fbd2b35b0c",
+    "url": "/donationreactwebsite/index.html"
+  },
+  {
+    "revision": "b5a413f559cfa7d5be88",
+    "url": "/donationreactwebsite/static/css/main.a260dcf6.chunk.css"
+  },
+  {
+    "revision": "34ddb5e66e78aa4491a9",
+    "url": "/donationreactwebsite/static/js/2.f5cf323b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/donationreactwebsite/static/js/2.f5cf323b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a413f559cfa7d5be88",
+    "url": "/donationreactwebsite/static/js/main.9e04014e.chunk.js"
+  },
+  {
+    "revision": "443cebd4dca37744fb09",
+    "url": "/donationreactwebsite/static/js/runtime-main.e1a6eddd.js"
+  },
+  {
+    "revision": "7a8b0d5c206e34571abf0263f2914f7f",
+    "url": "/donationreactwebsite/static/media/IMG_20200923_153803_560.7a8b0d5c.jpg"
+  },
+  {
+    "revision": "a498077667dce3184b5d73811ffa49f3",
+    "url": "/donationreactwebsite/static/media/Screen+Shot+2020-06-07+at+12.31.30+PM.a4980776.png"
+  },
+  {
+    "revision": "3b79df52d45233762884846a42b615a7",
+    "url": "/donationreactwebsite/static/media/Untitled-design-7.3b79df52.png"
+  },
+  {
+    "revision": "dd027d5c9c825715b37a016e449b71fe",
+    "url": "/donationreactwebsite/static/media/donatelogo.dd027d5c.png"
+  },
+  {
+    "revision": "04836c6a0d910be5e0d8d1b89ddfe37b",
+    "url": "/donationreactwebsite/static/media/orphan-reservation-Maharashtra.04836c6a.jpg"
+  }
+]);
